5becbced1279aea91ae873bfdfa2549bc5faaa9b
Daniel Haro Contreras <daniharopadul@gmail.com>
2022-05-31 00:01:12 +0200

    Add 'Encode-Sans' font-family

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,27 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+
+// 2. Extend the theme to include custom colors, fonts, etc
+const colors = {
+  brand: {
+    900: "#ad2938",
+    800: "#b42d3e",
+    700: "#D42A40",
+  },
+};
+
+const fonts = {
+  body: "Encode-Sans, sans-serif",
+  heading: "Georgia, serif",
+  mono: "Menlo, monospace",
+};
+
+const theme = extendTheme({ colors, fonts });
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
   );
