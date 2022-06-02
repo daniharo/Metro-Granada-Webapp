12e19a2e40cd06e07c7834d37423051a89294ca3
Daniel Haro Contreras <daniharopadul@gmail.com>
2022-06-02 02:01:58 +0200

    Add useGeolocation types

---
+++ src/hooks/useGeolocation.d.ts
@@ -0,0 +1,9 @@
+export interface EnrichedGeolocationCoordinates extends GeolocationCoordinates {
+  timestamp: GeolocationPosition["timestamp"] | null;
+  error: GeolocationPositionError | null;
+}
+
+export default function useGeolocation(
+  positionOptions?: PositionOptions,
+  callback?: (geolocation: EnrichedGeolocationCoordinates) => void
+): EnrichedGeolocationCoordinates;
