e009a259fccef3674213028f3f9fd73f00b6c528
Daniel Haro Contreras <daniharopadul@gmail.com>
2022-06-07 23:45:10 +0200

    Specify language in <html> tag

---
+++ pages/_document.tsx
@@ -0,0 +1,13 @@
+import { Head, Html, Main, NextScript } from "next/document";
+
+const MyDocument = () => (
+  <Html lang="es">
+    <Head />
+    <body>
+      <Main />
+      <NextScript />
+    </body>
+  </Html>
+);
+
+export default MyDocument;
